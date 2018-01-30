6988ecb1abfb375a15606d8acf0abf64ae9e221b
chutoan <chuvantoan1996hy@gmail.com>
2018-01-30 08:37:35 +0700

    add even-odd

--- Start-javaweb/src/main/java/Hello.java
+++ Start-javaweb/src/main/java/Hello.java
@@ -1,5 +1,13 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello");
+
+        int i = 100;
+        if (i % 2 == 0){
+            System.out.println("is even");
+        }else {
+            System.out.println("odd");
+        }
     }
 }
+
